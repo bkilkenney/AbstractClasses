3e2612364584a20134572f4b47fea83e158c0de3
Brad Kilkenney <bkilkenney@gmail.com>
2016-12-30 17:17:35 -0500

    Initial Commit

---
+++ WhyUseAbstractClasses/ContractEmployee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhyUseAbstractClasses
+{
+    class ContractEmployee : BaseEmployee
+    {
+
+        public int HourlyWage { get; set; }
+        public int TotalHours { get; set; }
+
+        public override int GetMonthlySalary()
+        {
+            return (this.HourlyWage * this.TotalHours);
+        }
+
+    }
+}
---
+++ WhyUseAbstractClasses/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Use Abstract classes when there is common functionality in derived classes
+//2. D-R-Y w/o abrstract BaseEmployee you'd have to change all the different employee types
+//3. Do NOT want to instantiate the BaseEmployee class, Abstract prevents this
+
+namespace WhyUseAbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FullTimeEmployee fte = new FullTimeEmployee()
+            {
+                ID = 101,
+                FirstName = "Brad",
+                LastName = "Kilkenney",
+                AnnualSalary = 65000              
+            };
+
+            Console.WriteLine(fte.GetFullName());
+            Console.WriteLine(fte.GetMonthlySalary());
+
+            Console.WriteLine("----------------------");
+
+            ContractEmployee cte = new ContractEmployee()
+            {
+                ID = 102,
+                FirstName = "Barry",
+                LastName = "Sanders",
+                HourlyWage = 25,
+                TotalHours = 160
+            };
+
+            Console.WriteLine(cte.GetFullName());
+            Console.WriteLine(cte.GetMonthlySalary());
+        }
+    }
+}
